841ac9ee2836a6af6df3741b9b72c529719e60c6
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2025-03-24 01:15:02 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+// import Login from './pages/Login';
+// import Register from './pages/Register';
+import './index.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/home" element={<Home />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/register" element={<Register />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
