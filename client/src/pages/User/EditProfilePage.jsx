e28a8b5077de5a9d37e78bf8385134002183e370
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2025-03-28 21:14:18 +0530

    Create EditProfilePage.jsx

---
+++ client/src/pages/User/EditProfilePage.jsx
@@ -0,0 +1,13 @@
+// EditProfilePage.jsx
+import React from 'react';
+import EditProfile from '../../components/User/EditProfile';
+
+const EditProfilePage = () => {
+  return (
+    <div>
+      <EditProfile />
+    </div>
+  );
+};
+
+export default EditProfilePage;
