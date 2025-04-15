b329bb0eaaeeb623806cadc0e70dcc2b2fccc0cb
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2025-04-15 10:19:05 +0530

    Create QuestionRepository.java

---
+++ server/src/main/java/com/nexora/server/repository/QuestionRepository.java
@@ -0,0 +1,14 @@
+package com.nexora.server.repository;
+
+import com.nexora.server.model.Question;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionRepository extends MongoRepository<Question, String> {
+    List<Question> findByTagsContaining(String tag);
+    List<Question> findByTitleContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String title, String description);
+    List<Question> findByIsFlaggedTrue();
+}
