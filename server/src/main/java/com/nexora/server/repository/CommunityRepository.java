629bdd92e65ba06272633eae01543da563b6dbc4
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2025-04-19 06:26:39 +0530

    Delete CommunityRepository.java

--- server/src/main/java/com/nexora/server/repository/CommunityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nexora.server.repository;
-
-import com.nexora.server.model.Community;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommunityRepository extends MongoRepository<Community, String> {
-}
