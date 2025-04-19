2601427ac5279775d525d8c435bcaf86e961bd00
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2025-04-19 07:16:49 +0530

    Update NotificationRepository.java

--- server/src/main/java/com/nexora/server/repository/forum/NotificationRepository.java
+++ server/src/main/java/com/nexora/server/repository/forum/NotificationRepository.java
@@ -1,4 +1,3 @@
-<<<<<<<< HEAD:server/src/main/java/com/nexora/server/repository/forum/NotificationRepository.java
 package com.nexora.server.repository.forum;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -8,14 +7,7 @@ import com.nexora.server.model.forum.Notification;
 
 import java.util.List;
 
-========
-package com.nexora.server.repository.post;
-
-import com.nexora.server.model.post.Notification;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
->>>>>>>> origin/main:server/src/main/java/com/nexora/server/repository/post/NotificationRepository.java
 @Repository
 public interface NotificationRepository extends MongoRepository<Notification, String> {
+    List<Notification> findByUserIdAndIsReadFalse(String userId);
 }
