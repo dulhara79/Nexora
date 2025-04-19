a20056f4638e1472bdccc76f4375535507579338
LAYATHMA B M A S <152529793+SENUVI20@users.noreply.github.com>
2025-04-19 06:25:37 +0530

    Create NotificationRepository.java

---
+++ server/src/main/java/com/nexora/server/repository/post/NotificationRepository.java
@@ -0,0 +1,9 @@
+package com.nexora.server.repository.post;
+
+import com.nexora.server.model.post.Notification;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends MongoRepository<Notification, String> {
+}
