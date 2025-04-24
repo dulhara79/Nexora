ca5807815569bd08b4e0e60542dd4e01e584ae15
Dewdu Sendanayake <162711007+DewduSendanayake@users.noreply.github.com>
2025-04-24 23:16:10 +0530

    Delete LearningPlanRepository.java

--- server/src/main/java/com/nexora/server/repository/LearningPlanRepository.java
+++
@@ -1,7 +0,0 @@
-// package com.nexora.server.repository;
-
-// import com.nexora.server.model.LearningPlan;
-// import org.springframework.data.mongodb.repository.MongoRepository;
-
-// public interface LearningPlanRepository extends MongoRepository<LearningPlan, String> {
-// }
