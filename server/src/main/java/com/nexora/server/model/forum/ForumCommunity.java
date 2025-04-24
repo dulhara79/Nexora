257167bd5d696d127a281aa8a4a449affc7e79ae
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2025-04-24 12:37:25 +0530

    Create ForumCommunity.java

---
+++ server/src/main/java/com/nexora/server/model/forum/ForumCommunity.java
@@ -0,0 +1,17 @@
+package com.nexora.server.model.forum;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "communities")
+public class ForumCommunity {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private int members;
+    private String icon;
+    private String color;
+}
