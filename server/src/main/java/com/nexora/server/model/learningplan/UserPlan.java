f94555de1537513a0152a3b1e79613d587365654
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2025-04-26 12:32:06 +0530

    Update UserPlan.java

--- server/src/main/java/com/nexora/server/model/learningplan/UserPlan.java
+++ server/src/main/java/com/nexora/server/model/learningplan/UserPlan.java
@@ -17,4 +17,60 @@ public class UserPlan {
     private String description;
     private String image;
     private List<Recipe> recipes;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getCuisineName() {
+        return cuisineName;
+    }
+
+    public void setCuisineName(String cuisineName) {
+        this.cuisineName = cuisineName;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public List<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(List<Recipe> recipes) {
+        this.recipes = recipes;
+    }
 }
