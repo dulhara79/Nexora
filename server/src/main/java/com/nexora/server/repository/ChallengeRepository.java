ce67632e8a93c56f201fc2443943ceb490329a25
Uvindu Seneviratne <uvisends24946@gmail.com>
2025-04-26 12:28:53 +0530

    Update ChallengeRepository.java

--- server/src/main/java/com/nexora/server/repository/ChallengeRepository.java
+++ server/src/main/java/com/nexora/server/repository/ChallengeRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 
+
 @Repository
 public interface ChallengeRepository extends MongoRepository<Challenge, String> {
     // Custom queries can be added if needed
