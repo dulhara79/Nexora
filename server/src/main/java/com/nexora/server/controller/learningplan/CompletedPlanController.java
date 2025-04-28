eb4d1b2dc868eb127cb45c3c36f2d8ab8f3f896c
Dewdu Sendanayake <162711007+DewduSendanayake@users.noreply.github.com>
2025-04-29 00:01:22 +0530

    Update CompletedPlanController.java

--- server/src/main/java/com/nexora/server/controller/learningplan/CompletedPlanController.java
+++ server/src/main/java/com/nexora/server/controller/learningplan/CompletedPlanController.java
@@ -1,22 +1,60 @@
+// // src/main/java/com/nexora/server/controller/learningplan/CompletedPlanController.java
+// package com.nexora.server.controller.learningplan;
+
+// import com.nexora.server.model.learningplan.CompletedPlan;
+// import com.nexora.server.repository.learningplan.CompletedPlanRepository;
+// import org.springframework.web.bind.annotation.*;
+// import java.util.List;
+
+// @RestController
+// @RequestMapping("/api/completedplans")
+// @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+// public class CompletedPlanController {
+//   private final CompletedPlanRepository repo;
+//   public CompletedPlanController(CompletedPlanRepository repo) {
+//     this.repo = repo;
+//   }
+
+//   @GetMapping
+//   public List<CompletedPlan> getCompleted(@RequestParam String userId) {
+//     return repo.findByUserId(userId);
+//   }
+// }
+
+
+
+
+
+//REST
+
 // src/main/java/com/nexora/server/controller/learningplan/CompletedPlanController.java
 package com.nexora.server.controller.learningplan;
 
 import com.nexora.server.model.learningplan.CompletedPlan;
 import com.nexora.server.repository.learningplan.CompletedPlanRepository;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
-@RequestMapping("/api/completedplans")
+@RequestMapping("/api/users/{userId}/completed-plans")
 @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class CompletedPlanController {
-  private final CompletedPlanRepository repo;
-  public CompletedPlanController(CompletedPlanRepository repo) {
-    this.repo = repo;
-  }
 
-  @GetMapping
-  public List<CompletedPlan> getCompleted(@RequestParam String userId) {
-    return repo.findByUserId(userId);
-  }
+    private final CompletedPlanRepository repo;
+
+    public CompletedPlanController(CompletedPlanRepository repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping(produces = "application/json")
+    public ResponseEntity<List<CompletedPlan>> getCompletedPlans(@PathVariable String userId) {
+        List<CompletedPlan> completedPlans = repo.findByUserId(userId);
+        return ResponseEntity.ok()
+            .cacheControl(CacheControl.maxAge(300, TimeUnit.SECONDS).mustRevalidate())
+            .body(completedPlans);
+    }
 }
