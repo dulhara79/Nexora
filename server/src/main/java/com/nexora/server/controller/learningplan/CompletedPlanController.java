772690a5f6e91e9f501343078978d093b308d149
Dewdu Sendanayake <162711007+DewduSendanayake@users.noreply.github.com>
2025-04-29 15:48:35 +0530

    Update CompletedPlanController.java

--- server/src/main/java/com/nexora/server/controller/learningplan/CompletedPlanController.java
+++ server/src/main/java/com/nexora/server/controller/learningplan/CompletedPlanController.java
@@ -28,10 +28,52 @@
 //REST
 
 // src/main/java/com/nexora/server/controller/learningplan/CompletedPlanController.java
+// package com.nexora.server.controller.learningplan;
+
+// import com.nexora.server.model.learningplan.CompletedPlan;
+// import com.nexora.server.repository.learningplan.CompletedPlanRepository;
+// import org.springframework.http.CacheControl;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
+
+// import java.util.List;
+// import java.util.concurrent.TimeUnit;
+
+// @RestController
+// @RequestMapping("/api/users/{userId}/completed-plans")
+// @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+// public class CompletedPlanController {
+
+//     private final CompletedPlanRepository repo;
+
+//     public CompletedPlanController(CompletedPlanRepository repo) {
+//         this.repo = repo;
+//     }
+
+//     @GetMapping(produces = "application/json")
+//     public ResponseEntity<List<CompletedPlan>> getCompletedPlans(@PathVariable String userId) {
+//         List<CompletedPlan> completedPlans = repo.findByUserId(userId);
+//         return ResponseEntity.ok()
+//             .cacheControl(CacheControl.noCache())
+//             .body(completedPlans);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
 package com.nexora.server.controller.learningplan;
 
 import com.nexora.server.model.learningplan.CompletedPlan;
 import com.nexora.server.repository.learningplan.CompletedPlanRepository;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -39,22 +81,42 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/api/users/{userId}/completed-plans")
 @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class CompletedPlanController {
 
     private final CompletedPlanRepository repo;
+    private final CompletedPlanModelAssembler assembler;
 
-    public CompletedPlanController(CompletedPlanRepository repo) {
+    public CompletedPlanController(
+        CompletedPlanRepository repo,
+        CompletedPlanModelAssembler assembler
+    ) {
         this.repo = repo;
+        this.assembler = assembler;
     }
 
     @GetMapping(produces = "application/json")
-    public ResponseEntity<List<CompletedPlan>> getCompletedPlans(@PathVariable String userId) {
-        List<CompletedPlan> completedPlans = repo.findByUserId(userId);
+    public ResponseEntity<CollectionModel<EntityModel<CompletedPlan>>> getCompletedPlans(
+        @PathVariable String userId
+    ) {
+        List<EntityModel<CompletedPlan>> plans = repo.findByUserId(userId).stream()
+            .map(assembler::toModel)
+            .toList();
+
+        CollectionModel<EntityModel<CompletedPlan>> collection = CollectionModel.of(
+            plans,
+            linkTo(methodOn(CompletedPlanController.class)
+                .getCompletedPlans(userId))
+            .withSelfRel()
+        );
+
         return ResponseEntity.ok()
             .cacheControl(CacheControl.noCache())
-            .body(completedPlans);
+            .body(collection);
     }
 }
