d2903a0f677db3070ad88377c10c510a6ab2d02c
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2025-05-13 12:58:12 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,40 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// Polyfill Node.js globals
+import buffer from "rollup-plugin-node-polyfills";
+import globals from "rollup-plugin-node-globals";
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(),NodeGlobalsPolyfillPlugin({
+      process: true,
+      buffer: true,
+    }),
+    NodeModulesPolyfillPlugin(),
+  ],
+  resolve: {
+    alias: {
+      crypto: "crypto-browserify",
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis", // Ensure globalThis is used
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [rollupNodePolyfills()],
+    },
+  },
 });
