3568a3d5fbbcb956097a9e9cf3d41c4e05554ab7
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2025-05-16 03:00:37 +0530

    Update UserResponse.java

--- server/src/main/java/com/nexora/server/controller/UserResponse.java
+++ server/src/main/java/com/nexora/server/controller/UserResponse.java
@@ -2,4 +2,4 @@ package com.nexora.server.controller;
 
 import java.util.Map;
 
-public record UserResponse(String id, String email, String name, String token, Map<String, String> _links) {}
+public record UserResponse(String id, String email, String name, String profilePhotoUrl, String token, Map<String, String> _links) {}
