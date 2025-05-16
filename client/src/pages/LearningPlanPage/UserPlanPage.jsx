09d44919bb8e02d57a5291dadc3edd3846f47604
Dewdu Sendanayake <162711007+DewduSendanayake@users.noreply.github.com>
2025-05-16 17:08:04 +0530

    Update UserPlanPage.jsx

--- client/src/pages/LearningPlanPage/UserPlanPage.jsx
+++ client/src/pages/LearningPlanPage/UserPlanPage.jsx
@@ -409,7 +409,7 @@ const UserPlanPage = () => {
                     <img src={r.image} alt={r.name} className="w-full h-80 object-cover rounded-md my-2" />
                     <button
                       onClick={() => handleRecipeDone(plan.id, r.name)}
-                      className={`mt-2 px-4 py-1 rounded text-white ${r.isDone ? 'bg-yellow-300' : 'bg-yellow-600 hover:bg-yellow-700'}`}>
+                      className={`mt-2 px-4 py-1 rounded text-white ${r.isDone ? 'bg-green-300' : 'bg-green-600 hover:bg-green-700'}`}>
                       {r.isDone ? '🎯 Done' : 'Mark as Done'}
                     </button>
                   </div>
