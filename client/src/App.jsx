b5023cc5294a4165fcb9dda5192fb46109fd3a0c
Uvindu Seneviratne <uvisends24946@gmail.com>
2025-05-17 01:32:35 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -53,10 +53,6 @@ import ChallengeDetail from "./pages/Challenge/ChallengeDetail";
 import ChallengeList from "./pages/Challenge/ChallengeList";
 import CreateChallenge from "./pages/Challenge/CreateChallenge";
 import StartChallenge from "./pages/Challenge/StartChallenge";
-import CollaborationHub from "./pages/Challenge/CollabHub";
-import Leaderboard from "./pages/Challenge/Leaderboard";
-import RemixGallery from "./pages/Challenge/ChallenfeRemix";
-import RecipeGenerator from "./pages/Challenge/RecipeGenerator";
 import CompletedChallenges from "./pages/Challenge/Complt Challenge";
 
 // Test Page
@@ -141,10 +137,6 @@ function App() {
           <Route path="/create-challenge" element={<CreateChallenge />} />
           <Route path="/edit-challenge/:challengeId" element={<CreateChallenge />} />
           <Route path="/start-challenge/:challengeId" element={<StartChallenge />} />
-          <Route path="/collaboration-hub" element={<CollaborationHub />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
-          <Route path="/remix-gallery" element={<RemixGallery />} />
-          <Route path="/recipe-generator" element={<RecipeGenerator />} />
           <Route path="/completed-challenges" element={<CompletedChallenges />} />
 
 
