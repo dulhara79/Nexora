828a4e3fcd6ac0e3c6948ba1f473555ca1e6981b
Dewdu Sendanayake <162711007+DewduSendanayake@users.noreply.github.com>
2025-05-16 21:57:25 +0530

    Update App.jsx
    Organized the page properly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,7 +26,6 @@ import RecipePage from "./pages/LearningPlanPage/RecipePage";
 import CuisinePage from "./pages/LearningPlanPage/CuisinePage";
 import UserPlanPage from "./pages/LearningPlanPage/UserPlanPage";
 import ProgressPage from "./pages/LearningPlanPage/ProgressPage";
-// import ShoppingListPage from "./pages/LearningPlanPage/ShoppingListPage";
 import KitchenWisdomPage from "./pages/LearningPlanPage/KitchenWisdomPage";
 
 // Forum Pages
@@ -95,8 +94,6 @@ function App() {
           <Route path="/userplan" element={<UserPlanPage />} />
           <Route path="/progress" element={<ProgressPage />} />
           <Route path="/progress/:userId" element={<ProgressPage />} />
-          {/* <Route path="/shoppinglist" element={<ShoppingListPage />} />
-          <Route path="/shoppinglist/:userId" element={<ShoppingListPage />} /> */}
           <Route path="/kitchenwisdom" element={<KitchenWisdomPage />} />
 
           {/* Forum Routes */}
@@ -110,10 +107,7 @@ function App() {
           <Route path="/forum/tags/:tag" element={<TagPage />} />
           <Route path="/forum/community" element={<ForumCommunityPage />} />
           <Route path="/forum/notifications" element={<ForumNotification />} />
-          <Route
-            path="/forum/community/:communityId"
-            element={<ForumCommunityPage />}
-          />
+          <Route path="/forum/community/:communityId" element={<ForumCommunityPage />} />
           <Route path="/forum/my-questions" element={<UserQuestionsPage />} />
           <Route path="/forum/quizzes" element={<QuizList />} />
           <Route path="/forum/quizzes/create" element={<QuizCreate />} />
@@ -126,16 +120,16 @@ function App() {
           <Route path="/create-post" element={<CreatePost />} />
           <Route path="/post-notifications" element={<Notifications />} />
           <Route path="/saved-posts" element={<SavedPosts />} />
+
           {/* Challenges Routes */}
           <Route path="/challenges" element={<ChallengeList />} />
           <Route path="/challenge/:challengeId" element={<ChallengeDetail />} />
           <Route path="/create-challenge" element={<CreateChallenge />} />
-          <Route
-            path="/edit-challenge/:challengeId"
-            element={<CreateChallenge />}
-          />
+          <Route path="/edit-challenge/:challengeId" element={<CreateChallenge />} />
+          
           {/* Test Page */}
           {/* <Route path="/test" element={<TestPage />} /> */}
+          
           {/* Challenges Routes */}
           <Route path="/challenges" element={<ChallengeList />} />
           <Route path="/challenge/:challengeId" element={<ChallengeDetail />} />
@@ -152,8 +146,10 @@ function App() {
           <Route path="/leaderboard" element={<Leaderboard />} />
           <Route path="/remix-gallery" element={<RemixGallery />} />
           <Route path="/recipe-generator" element={<RecipeGenerator />} />
+          
           {/* 404 Page */}
           <Route path="/*" element={<PageNotFound />} />
+          
         </Routes>
       </AuthProvider>
     </Router>
